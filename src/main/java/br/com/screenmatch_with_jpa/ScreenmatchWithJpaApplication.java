a57d6a5a0d7fa02e9f99648012442a919da006f9
Vinicius Costa <vinicius.fonseca132@gmail.com>
2024-09-06 12:51:50 -0300

    first commit

---
+++ src/main/java/br/com/screenmatch_with_jpa/ScreenmatchWithJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.screenmatch_with_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/screenmatch_with_jpa/ScreenmatchWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.screenmatch_with_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
